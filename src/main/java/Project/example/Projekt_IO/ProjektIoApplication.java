e7caa3dabf5557f566e480a31b148331e7f7ff03
Wasgo097 <patrpak906@student.polsl.pl>
2019-10-15 10:05:51 +0200

    Konfiguracja projektu

---
+++ src/main/java/Project/example/Projekt_IO/ProjektIoApplication.java
@@ -0,0 +1,13 @@
+package Project.example.Projekt_IO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektIoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektIoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Project/example/Projekt_IO/ProjektIoApplicationTests.java
@@ -0,0 +1,16 @@
+package Project.example.Projekt_IO;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjektIoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
